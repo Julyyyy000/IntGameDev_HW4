fbbc6f816d0785eff3eb839271e458342ee179b6
June Shi <jjs833@nyu.edu>
2023-02-26 17:14:03 -0500

    implemented game over and winning text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,23 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public bool nextScene = false;
+
+    public GameObject player;
+    TextMeshProUGUI text;
+    public GameObject textObject;
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = textObject.GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Scene scene = SceneManager.GetActiveScene();
         Debug.Log(nextScene);
         if (nextScene)
         {
-            SceneManager.LoadScene("Level2");
+            if (scene.name == "Level1")
+            {
+                SceneManager.LoadScene("Level2");
+            }
+            else if (scene.name == "Level2")
+            {
+                text.text = "Congratulations!";
+                textObject.SetActive(true);
+            }
+        }
+
+        if (player.transform.position.y < -16f)
+        {
+            textObject.SetActive(true);
         }
     }
 }
