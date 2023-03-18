b859d302eb4dfe1d6729a1750a0c60c6754aef21
June Shi <jjs833@nyu.edu>
2023-03-18 19:22:33 -0400

    edited hit object

--- Assets/Scripts/hitObject.cs
+++ Assets/Scripts/hitObject.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class hitObject : MonoBehaviour
 {
+
+    public float moveDistance = 10f;
+
+    float maximumPosition;
+    float minimumPosition;
+
+    public float moveSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maximumPosition = transform.position.y + moveDistance;
+        minimumPosition = transform.position.y - moveDistance;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 newPos = transform.position;
+        if ((newPos.y >= maximumPosition && moveSpeed > 0) || (newPos.y <= minimumPosition && moveSpeed <0))
+        {
+            moveSpeed *= -1;
+        } else
+        {
+            newPos.y += moveSpeed * Time.deltaTime;
+            transform.position = newPos;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
